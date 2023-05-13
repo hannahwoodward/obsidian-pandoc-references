ff54ca8ce85b06e3acb17d93913bf04ce0f82a7a
Hannah Woodward <hannah@woodward.sh>
2023-05-13 16:54:01 +0100

    Update preview rendering to run off events instead of setTimeout

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { execa } from 'execa';
+import { EventEmitter } from 'node:events';
 import { Plugin } from 'obsidian';
 import { PandocReferencesSettingTab } from 'src/PandocReferencesSettingTab';
 import { PandocReferencesView, VIEW_TYPE_REFERENCE_LIST } from 'src/PandocReferencesView';
@@ -18,6 +19,7 @@ export default class PandocReferences extends Plugin {
     activeFileName: null;
     activeFileRefsHash: null;
     citationCount: 0;
+    emitter: null;
     sidebarReady: false;
     statusBar: null;
     settings: PandocReferencesSettings;
@@ -31,6 +33,7 @@ export default class PandocReferences extends Plugin {
             return new PandocReferencesView(leaf, this);
         });
 
+        this.emitter = new EventEmitter();
         this.statusBar = this.addStatusBarItem();
         this.sidebarReady = true;
 
@@ -38,18 +41,20 @@ export default class PandocReferences extends Plugin {
             return;
         }
 
-        this.registerEvent(
-            this.app.workspace.on('layout-change', async () => {
-                await this.renderPreview();
-            })
-        );
+        this.emitter.on('previewSectionReady', async () => {
+            const renderer = this.app.workspace.activeLeaf.view.previewMode.renderer;
+            for (let i = 0; i < renderer.sections.length; i++) {
+                if (!renderer.sections[i].rendered) {
+                    return;
+                }
+            }
 
-        // NB this does rendering on a section/paragraph basis, which doesn't
-        // play well with pandoc same author/year referencing in different sections
-        // this.registerMarkdownPostProcessor(async (element, context) => {
-        //     // console.log(element)
-        //     // await this.renderCitations(element, context);
-        // }, 10);
+            await this.renderPreview(renderer);
+        });
+
+        this.registerMarkdownPostProcessor((element, context) => {
+            this.emitter.emit('previewSectionReady');
+        }, 100);
     }
 
     async onunload() {
@@ -95,78 +100,39 @@ export default class PandocReferences extends Plugin {
         return stdout;
     }
 
-    //     async renderCitations(element, context) {
-    //         // TODO: fix error with same author + year references in different blocks are not differentiated
-    //         if (!context.el || !context.el.outerHTML) {
-    //             return;
-    //         }
-    //
-    //         try {
-    //             const elHtmlString = await this.runPandocCmd(
-    //                 this.settings.pandocArgs,
-    //                 '',
-    //                 { input: context.el.outerHTML }
-    //             );
-    //             const parser = new DOMParser();
-    //             const elHtml = parser.parseFromString(elHtmlString, 'text/html');
-    //             const refsHtml = elHtml.querySelector('#refs');
-    //             refsHtml && refsHtml.remove();
-    //             context.el.innerHTML = elHtml.body.firstElementChild.innerHTML;
-    //         } catch (e) {
-    //             console.error(e);
-    //         }
-    //     }
-
-    async renderPreview() {
-        await this.app.workspace.iterateRootLeaves(async (rootLeaf) => {
-            if (rootLeaf.type !== 'leaf') {
-                return;
-            }
-
-            const viewState = rootLeaf.getViewState();
-            if (viewState.type !== 'markdown' || viewState.state.mode !== 'preview') {
-                return;
-            }
-
-            // TODO: find better way than setTimeout to wait for previewEl.outerHTML to be rendered
-            await setTimeout(async () => {
-                try {
-                    const renderer = rootLeaf.view.currentMode.renderer;
-                    for (let i = 0; i < renderer.sections.length; i++) {
-                        if (renderer.sections[i].el.classList.contains('mod-header') || renderer.sections[i].el.classList.contains('mod-footer')) {
-                            continue;
-                        }
-                        renderer.sections[i].el.setAttribute('data-section-id', i);
-                    }
-                    const elHtmlString = await this.runPandocCmd(
-                        this.settings.pandocArgs,
-                        '',
-                        { input: renderer.previewEl.outerHTML }
-                    );
-                    const parser = new DOMParser();
-                    const elHtml = parser.parseFromString(elHtmlString, 'text/html');
-                    const refsHtml = elHtml.querySelector('#refs');
-                    refsHtml && refsHtml.remove();
-
-                    for (let i = 0; i < renderer.sections.length; i++) {
-                        if (renderer.sections[i].el.classList.contains('mod-header') || renderer.sections[i].el.classList.contains('mod-footer')) {
-                            continue;
-                        }
-
-                        const sectionId = renderer.sections[i].el.getAttribute('data-section-id');
-                        const newSectionHtml = elHtml.querySelector(`[data-section-id="${sectionId}"]`);
-                        if (newSectionHtml) {
-                            renderer.sections[i].el.innerHTML = newSectionHtml.innerHTML;
-                        }
-                    }
-
-                    renderer.rerender();
-                } catch (e) {
-                    console.error(e);
+    async renderPreview(renderer) {
+        try {
+            let sectionsHtml = '';
+            for (let i = 0; i < renderer.sections.length; i++) {
+                if (renderer.sections[i].el.classList.contains('mod-header') || renderer.sections[i].el.classList.contains('mod-footer')) {
+                    continue;
                 }
-            }, 10);
 
-            this.previewReady = true;
-        });
+                renderer.sections[i].el.setAttribute('data-section-id', i);
+                sectionsHtml += renderer.sections[i].el.outerHTML;
+            }
+
+            const elHtmlString = await this.runPandocCmd(
+                this.settings.pandocArgs,
+                '',
+                { input: `<div>${sectionsHtml}</div>` }
+            );
+            const parser = new DOMParser();
+            const elHtml = parser.parseFromString(elHtmlString, 'text/html');
+
+            for (let i = 0; i < renderer.sections.length; i++) {
+                if (renderer.sections[i].el.classList.contains('mod-header') || renderer.sections[i].el.classList.contains('mod-footer')) {
+                    continue;
+                }
+
+                const sectionId = renderer.sections[i].el.getAttribute('data-section-id');
+                const newSectionHtml = elHtml.querySelector(`[data-section-id="${sectionId}"]`);
+                if (newSectionHtml) {
+                    renderer.sections[i].el.innerHTML = newSectionHtml.innerHTML;
+                }
+            }
+        } catch (e) {
+            console.error(e);
+        }
     }
 }
